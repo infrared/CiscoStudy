0f761d1edbf4ca2f18d9cc2b0379e7257a5b892b
infrared <infrared@pickle.(none)>
2012-03-26 22:06:59 -0400

    stuff

--- lib/CiscoStudy.pm
+++ lib/CiscoStudy.pm
@@ -4,6 +4,7 @@ use Database::Main;
 use Dancer::Plugin::DBIC;
 use List::Util 'shuffle';
 use Dancer ':syntax';
+use Array::Compare;
 
 our $VERSION = '0.1';
 
@@ -25,9 +26,47 @@ get '/subnetting-how-to' => sub {
 	template 'subnetting.tt';
 };
 
-get '/quiz-cisco-multiple-choice' => sub {
+get '/cisco-quiz-multiple-choice' => sub {
 	
-	my $id = 24;
+	if (!session('start')) {
+		session start => time;
+	}
+	else {
+		my $now = time;
+		if ($now - session('start') > 86400) {
+			# it's been 24 hrs, restart the timer
+			# and clear the list of MC questions
+			session start => $now;
+			session list => undef;
+			
+		}
+		else {
+			# update the timer
+			session start => $now;
+			
+			
+		}
+	}
+	if (!session('list')) {
+		my $all = schema->resultset('MCQuestions')->search;
+		if ($all->count) {
+			my @list;
+			while (my $row = $all->next) {
+				push(@list,$row->mc_id);
+			}
+			session list => \@list;
+		}
+	}
+	
+	my $list = session('list');
+	
+	my $random = int rand (scalar @{ $list });
+	
+	my $id = @$list[$random];
+	splice(@$list, $random,1);
+	
+	session list => $list;
+	
 	
 	my $search = schema->resultset('MCQuestions')->find($id);
     if ($search) {
@@ -35,16 +74,41 @@ get '/quiz-cisco-multiple-choice' => sub {
 		var question => $search->question;
         my $options = schema->resultset('MCOptions')->search({ parent_id => $id });
         my @ids;
-        my $hash = {};
+        
         while(my $row = $options->next) {
-			push(@ids,$row->mco_id);
-            $hash->{$row->mco_id}{option} = $row->options;
+			my $hash;
+			$hash->{id} = $row->mco_id;
+			$hash->{option} = $row->options;
+			push(@ids,$hash);
+           
         }
         my @shuffled = shuffle(@ids);
+		var id => $id;
 		var options => \@shuffled;
+		use Data::Dumper;
+		var list => Dumper session('list');
 	}
 	template 'quiz-multiple-choice';
 	
+};
+post '/cisco-quiz-multiple-choice' => sub {
+	my $id = param 'id';
+	my $guess = param 'option';
+	
+	my @guesses;
+	
+	
+	my $search = schema->resultset('MCQuestions')->find($id);
+	my (@answers) = ($search->answer =~ /(\d+)/g);
+	
+	my $compare = Array::Compare->new;
+	
+	if ($compare->perm( $guesses, \@answers)) {
+		var correct => "1";
+		
+	}
+
+	template 'quiz-multiple-choice';
 };
 
 true;
